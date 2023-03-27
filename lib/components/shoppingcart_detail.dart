fe4311303212b2b281470b474a30434dccecc8f4
YouHyunKim <yuhyunk7@gmail.com>
2023-03-27 16:44:11 +0900

    shoppingcart_detail add

---
+++ lib/components/shoppingcart_detail.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class ShoppingCartDetail extends StatelessWidget {
+  const ShoppingCartDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
