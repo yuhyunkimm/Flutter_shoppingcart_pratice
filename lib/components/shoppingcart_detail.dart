ddf7cf326547f705b98fafc5f471c13963c726f9
YouHyunKim <yuhyunk7@gmail.com>
2023-03-27 17:47:26 +0900

    shopping_detail CupertinoAlertDialog add

--- lib/components/shoppingcart_detail.dart
+++ lib/components/shoppingcart_detail.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_shoppingcart/constants.dart';
 
@@ -122,7 +123,22 @@ class ShoppingCartDetail extends StatelessWidget {
   Widget _buildDetailButton(BuildContext context) {
     return Align(
       child: TextButton(
-        onPressed: () {},
+        onPressed: () {
+          showCupertinoDialog(
+            context: context,
+            builder: (context) => CupertinoAlertDialog(
+              title: Text("장바구니에 담으시겠습니까?"),
+              actions: [
+                CupertinoDialogAction(
+                  child: Text("확인"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          );
+        },
         style: TextButton.styleFrom(
           backgroundColor: kAccentColor,
           minimumSize: Size(300, 50),
