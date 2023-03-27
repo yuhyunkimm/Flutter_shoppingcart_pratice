fea22667e38f97f79df88a5974324042834a4bdb
YouHyunKim <yuhyunk7@gmail.com>
2023-03-27 16:39:41 +0900

    theme add

---
+++ lib/theme.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_shoppingcart/constants.dart';
+
+ThemeData theme(){
+  return ThemeData(
+    primaryColor: kPrimaryColor,
+    scaffoldBackgroundColor: kPrimaryColor,
+  );
+}
