b66b610b1508d173b2c524bd1f9a401b867c3922
YouHyunKim <yuhyunk7@gmail.com>
2023-03-27 17:42:55 +0900

    shopping_detail add

--- lib/components/shoppingcart_detail.dart
+++ lib/components/shoppingcart_detail.dart
@@ -1,8 +1,7 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_shoppingcart/constants.dart';
 
 class ShoppingCartDetail extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -26,22 +25,116 @@ class ShoppingCartDetail extends StatelessWidget {
   }
 
   Widget _buildDetailNameAndPrice() {
-    return SizedBox();
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Urban Soft AL 10.0",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "\$699",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildDetailRatingAndReviewCount() {
-    return SizedBox();
+    return Padding(
+      padding: EdgeInsets.only(bottom: 20),
+      child: Row(
+        children: [
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Spacer(),
+          Text("review"),
+          Text("(26)", style: TextStyle(color: Colors.blue)),
+        ],
+      ),
+    );
   }
 
   Widget _buildDetailColorOptions() {
-    return SizedBox();
+    return Padding(
+      padding: EdgeInsets.only(bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Color Options"),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              _buildDetailIcon(Colors.black),
+              _buildDetailIcon(Colors.green),
+              _buildDetailIcon(Colors.orange),
+              _buildDetailIcon(Colors.grey),
+              _buildDetailIcon(Colors.white),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
-  Widget _buildDetailIcon(IconData mIcon) {
-    return SizedBox();
+  Widget _buildDetailIcon(Color mColor) {
+    return Padding(
+      padding: EdgeInsets.only(right: 10),
+      child: Stack(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(),
+              shape: BoxShape.circle,
+            ),
+          ),
+          Positioned(
+            left: 5,
+            top: 5,
+            child: ClipOval(
+              child: Container(
+                color: mColor,
+                width: 40,
+                height: 40,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildDetailButton(BuildContext context) {
-    return SizedBox();
+    return Align(
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+          backgroundColor: kAccentColor,
+          minimumSize: Size(300, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        child: Text(
+          "Add to Cart",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
   }
 }
