46a305e27dd56d3fb5848ae7ef1ce26711663b93
YouHyunKim <yuhyunk7@gmail.com>
2023-03-28 09:10:08 +0900

    appbar theme add

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,6 +4,10 @@ import 'package:flutter_shoppingcart/constants.dart';
 
 ThemeData theme(){
   return ThemeData(
+    appBarTheme: AppBarTheme(
+      backgroundColor: kPrimaryColor,
+      iconTheme: IconThemeData(color: Colors.black),
+    ),
     primaryColor: kPrimaryColor,
     scaffoldBackgroundColor: kPrimaryColor,
   );
